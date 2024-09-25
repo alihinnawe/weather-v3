18e54133a3672d19feff490eda7490650d424113
hinnaweali <hinnaweali@gmail.com>
2024-09-25 12:56:19 +0200

    add new date for each text

--- script/note-tab-controller.js
+++ script/note-tab-controller.js
@@ -5,6 +5,9 @@ import TabController from "../../../tool/tab-controller.js";
  */
 class NoteTabController extends TabController {
 
+    todayDateInput;
+    todayTimeInput;
+
     /**
      * Initializes a new instance.
      */
@@ -12,63 +15,76 @@ class NoteTabController extends TabController {
         super("notes");
 
         // Register controller event listeners
-		this.addEventListener("activated", event => this.processActivated());
-
-		// register controller event listeners
+        this.addEventListener("activated", event => this.processActivated());
     }
 
     // Get/set accessors
-	get newButton (){
-		return document.querySelector("head>template.note>div.control>button.new");
-	}
-	
-	get todayDate () {
-		return document.querySelector(".date");
+    get newButton() {
+        return this.center.querySelector("section.note>div.control>button.new");
+    }
 
-	}
-	
-	get todayTime () {
-		return document.querySelector(".time");
-
-}
-
+    get tableNote() {
+        return this.center.querySelector("section.note>div.notes>table>tbody");
+    }
 
     async processActivated() {
-        // Remove content of center article
+        // Clear the center article
         this.center.innerHTML = "";
         this.messageOutput.value = "";
 
+        // Clone the note section template and append it to the center
         const noteSectionTemplate = document.querySelector("template.note");
         const noteSectionOverview = noteSectionTemplate.content.firstElementChild.cloneNode(true);
-        this.center.append(noteSectionOverview); 
+        this.center.append(noteSectionOverview);
 
-        const todayDateInput = this.todayDate; 
-		const todayTimeInput = this.todayTime;
-        if (todayDateInput) {
-            let nowDate = new Date();
-            let date = nowDate.getFullYear() + '-' + (nowDate.getMonth() + 1).toString().padStart(2, '0') + '-' + nowDate.getDate().toString().padStart(2, '0');
-            todayDateInput.value = date; 
-            console.log(todayDateInput.value);
-        } else {
-            console.error("Date input not found!");
-        }
-		
-		if(todayTimeInput) {
-			let nowTime = new Date();
-			let time = nowTime.getHours().toString().padStart(2, '0') + ':' + nowTime.getMinutes().toString().padStart(2, '0');
-			todayTimeInput.value = time; 
-		}
-		
-		else {
-        console.error("Time input not found!");
-		}
+        // Call the method to set event listeners after the DOM has updated
+        this.setButtonEventListeners();
     }
+
+    setButtonEventListeners() {
+        const button = this.newButton;
+        if (button) {
+            button.addEventListener("click", () => this.newNotice());
+        } else {
+            console.error("New button not found!");
+        }
+    }
+
+	async newNotice() {
+
+		const noteTbodyTemplate = document.querySelector("template.note-row");
+		if (noteTbodyTemplate) {
+			const noteTbodyOverview = noteTbodyTemplate.content.firstElementChild.cloneNode(true);
+			this.tableNote.append(noteTbodyOverview);
+
+			const nowDate = new Date();
+			const date = nowDate.getFullYear() + '-' + (nowDate.getMonth() + 1).toString().padStart(2, '0') + '-' + nowDate.getDate().toString().padStart(2, '0');
+			const time = nowDate.getHours().toString().padStart(2, '0') + ':' + nowDate.getMinutes().toString().padStart(2, '0');	
+			const tableBodyDate = this.tableNote.querySelector("tr:last-child td input.date");	
+			const tableBodyTime = this.tableNote.querySelector("tr:last-child td input.time");
+
+			if (tableBodyDate) {
+				tableBodyDate.value = date;
+			} else {
+				console.error("Date input not found!");
+			}
+
+			if (tableBodyTime) {
+				tableBodyTime.value = time;
+			} else {
+				console.error("Time input not found!");
+			}
+		} else {
+			console.error("Note row template not found!");
+		}
+	}
+
 }
 
 /**
  * Register a listener for the window's "load" event.
  */
-window.addEventListener("load", event => {
+window.addEventListener("load", () => {
     const controller = new NoteTabController();
     console.log(controller);
 });
